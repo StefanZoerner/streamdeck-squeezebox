843ae01c58c494306809cf6cc841bce9bde2b91a
Stefan Zörner <stefan.zoerner@embarc.de>
2022-02-13 10:24:40 +0100

    refactoring of Constants

--- plugin/keyimages/albumart_images.go
+++ plugin/keyimages/albumart_images.go
@@ -5,28 +5,39 @@ import (
 	"fmt"
 	"github.com/nfnt/resize"
 	"image"
-	_ "image/jpeg"
+	_ "image/jpeg" // for JPEG images
 	"net/http"
 	"strconv"
 	"sync"
 )
 
 const (
-	ALBUM_ART_1x1 = "1x1"
-	ALBUM_ART_2x2 = "2x2"
-	ALBUM_ART_3x3 = "3x3"
+	AlbumArt1x1 = "1x1"
+	AlbumArt2x2 = "2x2"
+	AlbumArt3x3 = "3x3"
+
+	cacheImageSize = 300
 )
 
 var (
-	coverCache map[string]image.Image
-	lock       sync.Mutex
+	coverCache   map[string]image.Image
+	lock         sync.Mutex
+	defaultImage image.Image
 )
 
 func init() {
+	var err error
 	coverCache = make(map[string]image.Image)
+	defaultImage, err = GetImageByFilename("./assets/images/album_art_default.png")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func GetImageByUrl(url string) (image.Image, error) {
+
+	var img image.Image
+	var err error
 
 	lock.Lock()
 	defer lock.Unlock()
@@ -35,26 +46,28 @@ func GetImageByUrl(url string) (image.Image, error) {
 		return img, nil
 	}
 
-	res, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	if res.StatusCode != 200 {
-		return nil, errors.New(fmt.Sprintf("HTTP Status %d", res.StatusCode))
-	}
-	defer res.Body.Close()
-
-	img, _, err := image.Decode(res.Body)
-	if err != nil {
-
-		if img.Bounds().Max.X > 300 {
-			rect := image.Rectangle{
-				Min: image.Point{0, 300},
-				Max: image.Point{0, 300},
-			}
-			img, err = CropImage(img, rect)
+	if url == "" {
+		img = defaultImage
+	} else {
+		res, err := http.Get(url)
+		if err != nil {
+			return defaultImage, err
 		}
+		if res.StatusCode != 200 {
+			return defaultImage, fmt.Errorf("HTTP Status %d", res.StatusCode)
+		}
+		defer res.Body.Close()
 
+		img, _, err = image.Decode(res.Body)
+		if err != nil {
+			return defaultImage, err
+		}
+	}
+
+	if img != nil {
+		if img.Bounds().Max.X > cacheImageSize {
+			img = resize.Resize(cacheImageSize, cacheImageSize, img, resize.Lanczos3)
+		}
 		coverCache[url] = img
 	}
 
@@ -63,14 +76,14 @@ func GetImageByUrl(url string) (image.Image, error) {
 
 func ResizeAndCropImage(img image.Image, dimension string, tileNumber int) (image.Image, error) {
 	switch dimension {
-	case ALBUM_ART_1x1:
+	case AlbumArt1x1:
 		return resizeAndCropImageFor1x1(img)
-	case ALBUM_ART_2x2:
+	case AlbumArt2x2:
 		return resizeAndCropImageFor2x2(img, tileNumber)
-	case ALBUM_ART_3x3:
+	case AlbumArt3x3:
 		return resizeAndCropImageFor3x3(img, tileNumber)
 	default:
-		err := errors.New("Unknown dimension for album art")
+		err := errors.New("unknown dimension for album art")
 		return nil, err
 	}
 }
@@ -93,17 +106,13 @@ func resizeAndCropImageFor2x2(img image.Image, tileNumber int) (image.Image, err
 
 	switch tileNumber {
 	case 1:
-		crop, _ = CropImage(smallerImg, image.Rect(0, 0, StreamdeckTileSize-1, StreamdeckTileSize-1))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(0, 0, StreamdeckTileSize-1, StreamdeckTileSize-1))
 	case 2:
-		crop, _ = CropImage(smallerImg, image.Rect(72+19, 0, 163, 71))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(72+19, 0, 163, 71))
 	case 3:
-		crop, _ = CropImage(smallerImg, image.Rect(0, 72+19, 71, 163))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(0, 72+19, 71, 163))
 	case 4:
-		crop, _ = CropImage(smallerImg, image.Rect(72+19, 72+19, 163, 163))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(72+19, 72+19, 163, 163))
 	default:
 		crop = nil
 		err = errors.New("Illegal tile number for 2x2: " + strconv.Itoa(tileNumber))
@@ -121,34 +130,23 @@ func resizeAndCropImageFor3x3(img image.Image, tileNumber int) (image.Image, err
 
 	switch tileNumber {
 	case 1:
-		crop, _ = CropImage(smallerImg, image.Rect(0, 0, 71, 71))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(0, 0, 71, 71))
 	case 2:
-		crop, _ = CropImage(smallerImg, image.Rect(72+19, 0, 72+19+72, 71))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(72+19, 0, 72+19+72, 71))
 	case 3:
-		crop, _ = CropImage(smallerImg, image.Rect(72+19+72+19, 0, 72+19+72+19+72, 71))
-		break
-
+		crop, _ = cropImage(smallerImg, image.Rect(72+19+72+19, 0, 72+19+72+19+72, 71))
 	case 4:
-		crop, _ = CropImage(smallerImg, image.Rect(0, 71+19, 71, 71+19+71))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(0, 71+19, 71, 71+19+71))
 	case 5:
-		crop, _ = CropImage(smallerImg, image.Rect(72+19, 71+19, 72+19+72, 71+19+71))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(72+19, 71+19, 72+19+72, 71+19+71))
 	case 6:
-		crop, _ = CropImage(smallerImg, image.Rect(72+19+72+19, 71+19, 72+19+72+19+72, 71+19+71))
-		break
-
+		crop, _ = cropImage(smallerImg, image.Rect(72+19+72+19, 71+19, 72+19+72+19+72, 71+19+71))
 	case 7:
-		crop, _ = CropImage(smallerImg, image.Rect(0, 71+19+71+19, 71, 72+19+72+19+72))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(0, 71+19+71+19, 71, 72+19+72+19+72))
 	case 8:
-		crop, _ = CropImage(smallerImg, image.Rect(72+19, 71+19+71+19, 72+19+72, 72+19+72+19+72))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(72+19, 71+19+71+19, 72+19+72, 72+19+72+19+72))
 	case 9:
-		crop, _ = CropImage(smallerImg, image.Rect(72+19+72+19, 71+19+71+19, 72+19+72+19+72, 72+19+72+19+72))
-		break
+		crop, _ = cropImage(smallerImg, image.Rect(72+19+72+19, 71+19+71+19, 72+19+72+19+72, 72+19+72+19+72))
 	default:
 		crop = nil
 		err = errors.New("Illegal tile number for 3x3: " + strconv.Itoa(tileNumber))
